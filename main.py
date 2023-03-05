2eb6801059cbe487fc9cb2b0e86a1800790abc13
pgmrDoHan <61854396+pgmrDoHan@users.noreply.github.com>
2023-03-06 01:37:33 +0900

    [ADD] First Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+
+import modules.tjKoraoke as tj
+import modules.freeProxy as fp
+import modules.macro as macro
+
+links= []
+def logging(msg):
+    print("[Log]",msg)
+
+with open('setting.macro.json') as f:
+    settings = json.load(f)
+
+for song in settings['songsToRecommend']:
+    links.append(tj.get_recommendLink(
+        str(song['songType']),
+        str(song['singer']),
+        str(song['songTitle']),
+				str(song['songId']) if "songId" in song.keys() else 0
+    ))
+
+tjMacro = macro.tjKaraoke(links)
+
+if settings['howDo'] == "proxy":
+    freeProxy = fp.proxies(
+        str(settings['proxyType'])
+    )
+    proxies = freeProxy.get_list()
+    logging("Start MACRO.")
+    tjMacro.proxy(proxies)
+elif settings['howDo'] == "tor":
+    logging("Start MACRO.")
+    tjMacro.tor()
+elif settings['howDo'] == "all":
+    freeProxy = fp.proxies(
+        str(settings['proxyType'])
+    )
+    proxies = freeProxy.get_list()
+    logging("Proxies Count:"+str(len(proxies)))
+    logging("Start MACRO.")
+    tjMacro.proxy(proxies,str(settings['proxyType']))
+    tjMacro.tor()
